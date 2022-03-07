c8329d9166c01b603dfeda20ffa10959038e3e45
Alex <alexatrs@hotmail.co.uk>
2022-03-07 22:12:40 +0000

    redux added and login/register feature started.

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { useSelector } from "react-redux";  
 import './App.css';
+import { selectUser } from "./features/userSlice"
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Login from './Login';
 
 function App() {
+
+  const user = useSelector(selectUser)
   return (
     <div className="app">
         <Header />
 
-        {/* App Body */}
-        <div className="app__body">
+        {!user ? <Login /> : (
+          <div className="app__body">
           <Sidebar />
           <Feed />
           {/* Widgets */}
         </div>
+        )} 
+
+        
     </div>
   );
 }
