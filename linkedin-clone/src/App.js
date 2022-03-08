f0846ebc4275333bd67b3acac0fbb3c67a594ca2
Alex <alexatrs@hotmail.co.uk>
2022-03-08 19:41:41 +0000

    work on login auth.

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { useSelector } from "react-redux";  
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";  
 import './App.css';
-import { selectUser } from "./features/userSlice"
+import { login, logout, selectUser } from "./features/userSlice"
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  // const dispatch = useDispatch()
+  console.log(user);
 
-  const user = useSelector(selectUser)
+  // useEffect(() => {
+  //   auth.onAuthStateChanged(userAuth => {
+  //     if (userAuth) {
+  //       // User is logged in
+  //       dispatch(
+  //         login({
+  //         email: userAuth.email,
+  //         uid: userAuth.uid,
+  //         displayName: userAuth.displayName,
+  //         photoUrl: userAuth.photoURL,
+  //       })
+  //       );
+  //     } else {
+  //       // User is logged out
+  //       dispatch(logout());
+  //     }
+  //   })
+  // }, [])
+
   return (
     <div className="app">
         <Header />
