4cc307a1939c42b252099f25cb8b3a798f7bb101
Alex <alexatrs@hotmail.co.uk>
2022-03-14 20:13:40 +0000

    project set up and started.

---
+++ movie-quotes/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+
+      {/* Header */}
+      <Header />
+        {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Categories */}
+     
+    </div>
+  );
+}
+
+export default App;
